ac9b89923744db087f3067e756e9a1a88632d4f3
Ritika Lath <lathritika2@gmail.com>
2020-12-22 17:21:12 +0530

    InfoBoxes

--- src/India.js
+++ src/India.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect} from 'react';
 import './India.css';
-import { FormControl, Select, MenuItem, Card, CardContent, Button } from '@material-ui/core';
+import { FormControl, Select, MenuItem, Card, CardContent } from '@material-ui/core';
 import Navbar from './components/Navbar';
+import InfoBox from './InfoBox';
+import Indiamap from './Indiamap';
+import Table2 from './Table2'
 
 function India() {
 
@@ -9,6 +12,17 @@ function India() {
     
     const [states, setStates] = useState([]);
     const [state, setState] = useState('india');
+    const [stateInfo,setStateInfo] = useState({});
+    const[tableData,setTableData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://www.trackcorona.live/api/countries/in")
+        .then((response) => response.json())
+        .then((data) => {
+            setStateInfo(data);
+        });
+        
+    }, [])
   
     // UseEffects:-
     useEffect(() => {
@@ -26,7 +40,8 @@ function India() {
                     }
                 
                  ) );
-                
+
+                setTableData(data);
                 setStates(states);
             });
             
@@ -37,11 +52,26 @@ function India() {
 
     const onStateChange = async (event) => {
         const stateCode = event.target.value;
-        console.log("jamwn",stateCode)
-
         setState(stateCode);
-    }
-
+        const url = stateCode === 'india' ? 'https://www.trackcorona.live/api/countries/in' 
+        : `https://www.trackcorona.live/api/provinces/${stateCode}`
+        await fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            const stateInfo = data.data.map((location) => (
+                {
+                    confirmedCase : location.confirmed,
+                    deadCase : location.dead,
+                    recoveredCase : location.recovered
+                }
+            ));
+            setState(stateCode);
+            // setStateInfo(data);
+            setStateInfo(stateInfo)
+        });
+        
+        };
+        console.log("state info", stateInfo );
 
     return (
         <div className="india">
@@ -75,6 +105,36 @@ function India() {
 
             </div>
             <div className="india__stats">
+            
+            <InfoBox
+            // isRed
+            // active={casesType === "cases"}
+            // onClick={(e) => setCasesType("cases")} 
+            title="CoronaVirus Cases" 
+            // cases={stateInfo.map((location) => (
+            //     {locationconfirmedCase}
+            // ))} 
+            cases = {stateInfo.confirmedCase}
+            // total={prettyPrintStat(countryInfo.cases)}
+
+            />
+            <InfoBox
+            // active={casesType === "recovered"}
+            // onClick={(e) => setCasesType("recovered")}  
+            title="Recovered" 
+            // cases={prettyPrintStat(countryInfo.todayRecovered)} 
+            // total={prettyPrintStat(countryInfo.recovered)}
+
+            />
+            <InfoBox 
+            // isRed
+            // active={casesType === "deaths"}
+            // onClick={(e) => setCasesType("deaths")} 
+            title="Deaths" 
+            // cases={prettyPrintStat(countryInfo.todayDeaths)} 
+            // total={prettyPrintStat(countryInfo.deaths)}
+
+            />
                       {/* InfoBoxes */}
                         {/* InfoBoxes */}
                         {/* InfoBoxes */}
@@ -84,13 +144,20 @@ function India() {
 
            
                 {/* Map */}
+            <Indiamap />
                 
             {/* Prediction textboxes */}
             </div>
+            <Card className="india__right">
+                     <CardContent>
+                     <h1>Live cases by States</h1>
+                     </CardContent>
+            </Card>
         
           
 
             {/* Table */}
+            <Table2  states = {tableData}/>
 
            
 
---
+++ src/Indiamap.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Indiamap() {
+    return (
+        <div className="map">
+        <h1>Hi i am a map</h1>
+            
+        </div>
+    )
+}
+
+export default Indiamap
